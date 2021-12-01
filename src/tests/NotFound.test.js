f4572482a434929b75ec42ca6b9b38ff1f3f66ba
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2021-11-30 23:28:42 -0300

    feat: Feita a implementaçao dos testes requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Teste o componente <NotFound.js />', () => {
+  it('Teste se página contém um heading h2 com o texto Page requested not found.', () => {
+    renderWithRouter(<NotFound />);
+    const title = screen.getByRole(
+      'heading', { level: 2, name: /Page requested not found/i },
+    );
+    expect(title).toHaveTextContent('Page requested not found');
+  });
+
+  it('Teste se página mostra a imagem.', () => {
+    renderWithRouter(<NotFound />);
+    const img = screen.getByRole('img', {
+      name: 'Pikachu crying because the page requested was not found',
+    });
+    expect(img).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
